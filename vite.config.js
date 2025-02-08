8b87237fc8097cce531bc2664d152fd3c1d95e32
Fergus Fowler <fergusfowler@gmail.com>
2025-02-08 12:27:39 +0000

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: "/cosmic-lab/"
+  base: "/cosmic-lab/",
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        nested: 'nested/index.html',
+        planetDesigner: 'nested/planet-designer.html',
+        render: 'nested/render.html',
+        profile: 'nested/profile.html',
+        login: 'nested/login.html'
+      }
+    }
+  }
 });
