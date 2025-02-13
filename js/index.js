8bab59799840238179503ba6648135d90a932215
Fergus Fowler <fergusfowler@gmail.com>
2025-02-13 17:33:39 +0000

    removed rhe fukcing port number

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ async function SetUsername() {
 
     sessionId = sessionId.replace(/['"]+/g, '').toUpperCase();
 
-    const response = await fetch(`https://cosmiclabapi.co.uk:2030/api/LoginDetails/GetUsername?sessionId=${sessionId}`)
+    const response = await fetch(`https://cosmiclabapi.co.uk/api/LoginDetails/GetUsername?sessionId=${sessionId}`)
 
     label.innerText = await response.text();
 }
